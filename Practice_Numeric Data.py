86e9d46335dcaa4d44f112cf59ca138275ae6caf
Vrushabh Dengale <104447789+vrushabh-27@users.noreply.github.com>
2022-06-03 21:20:27 +0530

    Add files via upload

---
+++ Practice_Numeric Data.py
@@ -0,0 +1,33 @@
+#Practice
+#1.0 ask name of user and welcome him
+name=input("Enter your name:" )
+#print("Hello",name,'\nYou are welcome.')
+
+#2.0 ask his house number
+house_number=int(input('Enter your house number:'))
+#print('Your house number is:',house_number)
+
+#3.0 ask his percentage
+percent=float(input('Enter your percentage:'))
+#print('Your percentage is:',percent)
+
+#4.0
+print('Hello',name,'\nYou are welcome.\n Your house number is',house_number,'\nYour % is',percent)
+
+#5. assign variables and show their type
+
+a=5
+b=45.6
+c="hello"
+d=1+2j
+e=4
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+print(type(d),d)
+
+#6.0 Simple mathematical operations and finding data types of answers
+print((a+b),type(a+b))
+print((a-d),type(a-d))
+print((a*e),type(a*e))
+print((a/b),type(a/b))
